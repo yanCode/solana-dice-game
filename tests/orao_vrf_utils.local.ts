89663367935f7d649cd5f299d24b38be6a10d6c1
Zhang ShengYan <yan.code@gmail.com>
2024-11-10 19:19:32 +0800

    feat: validate is loaded locally

--- tests/orao_vrf_utils.local.ts
+++ tests/orao_vrf_utils.local.ts
@@ -1,17 +1,20 @@
 import { AnchorProvider, BN } from "@coral-xyz/anchor";
 import { FulfillBuilder, InitBuilder, Orao } from "@orao-network/solana-vrf";
-import { Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import nacl from "tweetnacl";
 
+const VRF_PROGRAM_ID = "VRFzZoJdhFWL8rkvu87LpKM3RbcVezpMEc6X5GVDr7y";
 
 export class OraoVrfHelper {
   private readonly vrf: Orao;
   private readonly treasury: Keypair;
   private readonly fulfillmentAuthority: Keypair;
   private readonly configAuthority: Keypair;
+  private readonly connection: AnchorProvider["connection"];
 
   private constructor(provider: AnchorProvider) {
     this.vrf = new Orao(provider);
+    this.connection = provider.connection;
     this.treasury = Keypair.generate();
     this.configAuthority = Keypair.generate();
     this.fulfillmentAuthority = Keypair.generate();
@@ -32,6 +35,13 @@ export class OraoVrfHelper {
   }
 
   async init(): Promise<void> {
+    const programInfo = await this.connection.getAccountInfo(
+      new PublicKey(VRF_PROGRAM_ID)
+    );
+    if (!programInfo) {
+      throw new Error("Orao VRF program is not loaded into your localnet, please check it...");
+    }
+    
     const fee = 2 * LAMPORTS_PER_SOL;
     const fulfillmentAuthorities = [this.fulfillmentAuthority.publicKey];
     const tx = await new InitBuilder(
@@ -43,7 +53,7 @@ export class OraoVrfHelper {
     ).rpc();
     console.log("Your transaction signature of InitBuilder:", tx);
   }
-  
+
   async mockFulfillment(seed: Buffer): Promise<void> {
     let signature = nacl.sign.detached(seed, this.fulfillmentAuthority.secretKey);
     await new FulfillBuilder(this.vrf, seed).rpc(
